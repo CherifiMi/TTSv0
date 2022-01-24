b4ca730e8d0abbadb831bd443fe4c9dae774fe0d
CherifiMi <cherifi154949@gmail.com>
2022-01-24 09:14:04 +0100

    test to speech

---
+++ Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Speech.Synthesis;
+using System.IO;
+
+namespace TTSBS
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string text = File.ReadAllText(@"D:\vs\TTSBS\1984.txt");
+
+            List<string> word = text.Split().ToList();
+
+            string words = string.Join("\n", word);
+
+            Console.WriteLine($"{words}");
+
+            var synthesizer = new SpeechSynthesizer();
+
+            synthesizer.SetOutputToDefaultAudioDevice();
+
+            synthesizer.SelectVoiceByHints(VoiceGender.Male);
+
+
+            synthesizer.Speak(text);
+
+
+            Console.ReadKey();
+        }
+    }
+}
+
